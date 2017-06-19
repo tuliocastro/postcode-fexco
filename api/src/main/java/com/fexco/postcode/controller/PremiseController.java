250781152c4c58b0c21178c9bac7b857c901aae4
Tulio Castro <tulio.ac88@gmail.com>
2017-06-19 08:46:19 -0300

    parse map params to object

--- api/src/main/java/com/fexco/postcode/controller/PremiseController.java
+++ api/src/main/java/com/fexco/postcode/controller/PremiseController.java
@@ -1,6 +1,8 @@
 package com.fexco.postcode.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fexco.postcode.base.controller.BaseController;
+import com.fexco.postcode.dto.request.PremiseRequest;
 import com.fexco.postcode.dto.response.PremiseResponse;
 import com.fexco.postcode.service.PremiseService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +20,10 @@ public class PremiseController extends BaseController {
     @RequestMapping(value = "{apiKey}/address/uk/{query}")
     public PremiseResponse[] findAddress(@PathVariable String apiKey, @PathVariable String query, @RequestParam Map<String, String> mapParams) {
 
-        return premiseService.findAdresses(apiKey, query, null);
+        ObjectMapper mapper = new ObjectMapper();
+        PremiseRequest configuration = mapper.convertValue(mapParams, PremiseRequest.class);
+
+        return premiseService.findAdresses(apiKey, query, configuration);
 
     }
 
