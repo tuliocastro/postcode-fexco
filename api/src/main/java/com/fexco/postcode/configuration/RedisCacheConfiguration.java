27f0440a9fe22af8b8636b42ca3c7dfa3d0aae6c
Tulio Castro <tulio.ac88@gmail.com>
2017-06-20 16:23:22 -0300

    irish endpoints

--- api/src/main/java/com/fexco/postcode/configuration/RedisCacheConfiguration.java
+++ api/src/main/java/com/fexco/postcode/configuration/RedisCacheConfiguration.java
@@ -1,8 +1,8 @@
-package com.fexco.postcode.cache;
+package com.fexco.postcode.configuration;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,9 +12,15 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
-public class RedisConfiguration extends CachingConfigurerSupport {
+public class RedisCacheConfiguration extends CachingConfigurerSupport {
 
-    private Logger logger = LoggerFactory.getLogger(RedisConfiguration.class);
+    private Logger logger = LoggerFactory.getLogger(RedisCacheConfiguration.class);
+
+    @Value("${cache.redis.hostname}")
+    private String hostname;
+
+    @Value("${cache.redis.port}")
+    private Integer port;
 
     @Bean
     @Order(20)
@@ -23,8 +29,8 @@ public class RedisConfiguration extends CachingConfigurerSupport {
         logger.info("Initializing Redis factory ...");
 
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setHostName("127.0.0.1");
-        jedisConnectionFactory.setPort(6379);
+        jedisConnectionFactory.setHostName(hostname);
+        jedisConnectionFactory.setPort(port);
         return jedisConnectionFactory;
     }
 
